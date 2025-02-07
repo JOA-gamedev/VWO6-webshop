9905832cc6514d3e71a116d836a6aebcf25f5db8
JOA-gamedev <Joachimpetrusi@gmail.com>
2025-02-07 13:24:03 +0100

    replaced database query to controller

--- app/controllers/items/producten.php
+++ app/controllers/items/producten.php
@@ -1,2 +1,9 @@
 <?php
-view("producten");
+
+// Database connection
+$db = new Database();
+
+// Fetch products from the database
+$producten = $db->query("SELECT * FROM producten")->fetchAll(PDO::FETCH_ASSOC);
+
+view("producten", ['producten' => $producten]); // Laad de view 'producten.view.php' en geef de variabele $producten mee
