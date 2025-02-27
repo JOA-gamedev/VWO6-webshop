f94b9b527bfe7be5803cb39488e2c3a650b8073a
jenscm1 <jm26734@psg.nl>
2025-02-27 13:44:45 +0100

    Update marquee message in topbar with promotional text and adjusted scroll speed

--- app/views/parts/topbar.view.php
+++ app/views/parts/topbar.view.php
@@ -1,7 +1,7 @@
 <div x-data="{ showTicker: true }" x-show="showTicker" class="bg-blue-600 text-white py-2 relative">
     <div class="container mx-auto">
-        <marquee behavior="scroll" direction="left">
-            Welkom bij onze Kledingwinkel! Bekijk onze nieuwste collectie en aanbiedingen!
+        <marquee behavior="scroll" direction="left" scrollamount="15">
+           // Welkom bij onze Kledingwinkel! // Bekijk onze nieuwste collectie en aanbiedingen! // Gebruik tijdelijk de code 'JOACHIM30' voor 30% korting op uw bestelling! //
         </marquee>
         <button @click="showTicker = false" class="absolute top-0 right-0 mt-2 mr-2 text-white">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
